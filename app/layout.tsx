9542066bde791a51f4a728b9c00c407f241588ee
mbakes1 <mbasasishuba2@gmail.com>
2025-07-22 13:18:25 +0200

    speed insights

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import type { Metadata } from "next";
 import { ThemeProvider } from "../components/provider";
 import "./globals.css";
 import { Analytics } from "@vercel/analytics/next";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 export const metadata: Metadata = {
   title: "BidBase - Discover & Manage RFQs and RFPs",
   description:
@@ -50,6 +51,7 @@ export default function RootLayout({
           {children}
           <Toaster />
           <Analytics />
+          <SpeedInsights />
         </ThemeProvider>
       </body>
     </html>
